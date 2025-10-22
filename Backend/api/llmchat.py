8ce24de3450dbd086444d3cdcad6e92c416e4d59
Ishan <ishan.sandhu3121@gmail.com>
2025-10-21 22:58:16 -0700

    back api prog

--- Backend/api/llmchat.py
+++ Backend/api/llmchat.py
@@ -1,14 +1,26 @@
 from google import genai
 from dotenv import load_dotenv
+from pydantic import BaseModel
 import os
-# fetching API from env
+# fetching API KEY from env
 load_dotenv()
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
+# input data validation
+
+class userQuery(BaseModel):
+    currentQuery: str
+
+# example of full chat
+fullChat = [{"role": "user", "parts": [{"text": "Explain how AI works in a few words"}]}, 
+{"role": "model", "parts": [{"text": "AI is a technology that allows machines to learn and make decisions based on data."}]},
+{"role": "user", "parts": [{"text": "Give me the chat history"}]}]
+
 
 client = genai.Client(api_key=GOOGLE_API_KEY)
 
 response = client.models.generate_content(
-    model="gemini-2.5-flash", contents="Explain how AI works in a few words"
+    model="gemini-2.5-flash", contents=fullChat
 )
 print(response.text)
 print(response)

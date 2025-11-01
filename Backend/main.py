0e0e09e408990294531a1265904ea6776c3b65cf
Ishan <ishan.sandhu3121@gmail.com>
2025-11-01 14:25:17 -0700

    got the chat input frontend and backend int working

--- Backend/main.py
+++ Backend/main.py
@@ -3,6 +3,7 @@ import redis
 from api.Routes.idroutes import router as idroute
 from api.Routes.chatroute import router as chatroute
 from api.redis.initialization import start_redis, shutdown_redis
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 
 @asynccontextmanager
@@ -16,3 +17,11 @@ async def lifespan(app: FastAPI):
 app = FastAPI(title="QA Chat API", lifespan=lifespan)
 app.include_router(idroute)
 app.include_router(chatroute)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+allow_headers=["*"],
+)
--- Frontend/src/Chat/input.jsx
+++ Frontend/src/Chat/input.jsx
@@ -1,19 +1,26 @@
 import { useState } from 'react'
 import './input.css'
 import { IoIosSend } from "react-icons/io";
-
+import axios from 'axios';
 
 export default function ChatInput(){
     const [query, setQuery] = useState("")
 
     return (
         <div className="chat-input">
-            <form 
+            <form
+            name="chat-input-form"
             className="chat-input-form"
+            onSubmit = {(e)=>{
+                e.preventDefault();
+                onSend({message: query});
+                setQuery('');
+            }}
             >
                 
             <textarea 
             className="chat-input-textarea" 
+            name="chat-input-textarea"
             type="text" 
             placeholder="Type your message here..." 
             onChange = {e => setQuery(e.target.value)} 
@@ -22,7 +29,7 @@ export default function ChatInput(){
             <button 
             className="chat-input-button" 
             type="submit"
-            onClick={() => onSend({message: query})}
+            // onClick={() => onSend({message: query})}
             disabled={!query.trim()}
             >
                 <IoIosSend size={30} color="#ffffff"/>
@@ -32,8 +39,16 @@ export default function ChatInput(){
   );
 }
 
-function onSend(props){
-    return(
-        alert(props.message)
-    )
+async function onSend(props){
+
+    try {
+        const queryRes = await axios.post('http://127.0.0.1:8000/chatResponse', {
+            currentQuery: props.message,
+            sessionId: 'test-session-id'
+        });
+        alert(queryRes.data.chatResponse);
+    }
+    catch(error){
+        console.error('Error:', error);
+    }
 }

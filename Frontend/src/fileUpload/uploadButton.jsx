46ca16926e3812b7bcaa7b5bdb992e24bbb1ae48
Ishan <ishan.sandhu3121@gmail.com>
2025-11-09 13:35:12 -0800

    got the upload button working, need to get the file reading and sending to backend working

--- Frontend/src/fileUpload/uploadButton.jsx
+++ Frontend/src/fileUpload/uploadButton.jsx
@@ -1,13 +1,26 @@
+import { useRef } from 'react';
+import './uploadFile.css';
 export default function UploadButton(){
+    const fileInputRef = useRef(null);
 
+    const handleFileUpload = (e) => {
+        const files = e.target.files;
+        if (files.length===0 || !files) return;
+        console.log(e.target);
+
+        const curFile = files[0];
+        console.log(curFile.name);
+        console.log(curFile.type)
+    }
     const onClickButton = () => {
-       
+        if(!fileInputRef.current) return;
+        fileInputRef.current.click();
     }
 
     return (
         <div>
-            <input type="file" display="none" />
-            <button onClick={onClickButton}>Upload</button>
+            <input ref={fileInputRef} type="file" accept="application/pdf" hidden onChange={handleFileUpload}  multiple />
+            <button onClick={onClickButton} className="upload-button">Upload</button>
         </div>
     )
 }

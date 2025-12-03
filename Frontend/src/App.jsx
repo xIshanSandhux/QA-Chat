a4037ce61a9818d19412bc3f75fa3685dc224891
Ishan <ishan.sandhu3121@gmail.com>
2025-12-02 22:38:50 -0800

    working on the search component UI

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,7 +6,6 @@ function App() {
 
   return (
     <>
-    {/* <UploadButton /> */}
       {/* <ChatMain /> */}
       <SearchMain />
     </>
--- Frontend/src/searchPDF/searchDisplay/searchBar.jsx
+++ Frontend/src/searchPDF/searchDisplay/searchBar.jsx
@@ -1,9 +1,46 @@
 import './searchBar.css';
+import {useState} from 'react';
+import { IoIosSearch } from "react-icons/io";
 export default function SearchBar(){
 
+    const [search, setSearch] = useState("");
+    // console.log(search);
+
     return (
-        <div className="search-bar">
-            <h1>Search component</h1>
+        <div className="search-main-container">
+            <form 
+            name="search-bar-form"
+            className="search-bar-main-container"
+            onSubmit={(e)=>{
+                e.preventDefault();
+                console.log(search);
+                setSearch("");
+                // will be sending the query to the backend
+                // need to get the backend working for it but will be doing console log until then
+            }}
+            >
+                <textarea
+                className="search-bar-input"
+                type="text"
+                spellCheck="true"
+                placeholder="Search in PDF..."
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                />
+
+                <button 
+                className="search-bar-button"
+                type="submit"
+                disabled={!search.trim()}
+                >
+                    <IoIosSearch size={30} color="#ffffff"/>
+                </button>
+            </form>
+
+            <h3>PDF Search Results: ---</h3>
+            {/* TODO: create function to update the results count based on the chunks received from backend. 
+            Maybe have "---" as the default value ? Maybe have a loading state ?*/}
+            {/* TODO: dsiplay the search results in a list format */}
         </div>
     )
 }

538c57768f4e9b0b9bab8c32b0b92a3834e99f66
Ishan <ishan.sandhu3121@gmail.com>
2025-12-06 12:16:51 -0800

    got the LLM abstraction working

--- Backend/LLM/Providers/groqProvider.py
+++ Backend/LLM/Providers/groqProvider.py
@@ -1,14 +1,13 @@
-from LLM.interface import LLMInterface
-from LLM.LLMConfig import get_LLMConfig
+from ..interface import LLMInterface
 from groq import AsyncGroq
 
 
 class GroqProvider(LLMInterface):
 
-    def __init__(self, api_key: str):
-        self.client = AsyncGroq(api_key=get_LLMConfig().groq_api_key)
-        self.model = get_LLMConfig().model
-    
+    def __init__(self, key: str, model: str):
+        self.client = AsyncGroq(api_key=key)
+        self.model = model
+
     async def generate_response(self, prompt: str) -> str:
         response = await self.client.chat.completions.create(
             model=self.model,
--- Backend/api/logic/llmchat.py
+++ Backend/api/logic/llmchat.py
@@ -5,31 +5,37 @@ import os
 import json
 from api.redis.chatupdates import addToChat, getChatHistory
 from vectorDB.chromaDB.chroma import getQueryChunks
-
+from LLM.factory import LLMProviderFactory
 # fetching API KEY from env
-load_dotenv()
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+# load_dotenv()
+# GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+# client = genai.Client(api_key=GOOGLE_API_KEY)
 
-client = genai.Client(api_key=GOOGLE_API_KEY)
+
+provider = LLMProviderFactory()
 
 async def generateResponse(session_id, query: str, rag:bool = False):
     
-    await addToChat(session_id, "user", query)
-    chat_history = await getChatHistory(session_id)
-    if rag:
-        system_prompt = open("api/logic/sysprompt.md", "r").read()
-        relChunks = await getQueryChunks(session_id, query)
-        chunks = json.dumps(relChunks["documents"])
-        system_prompt = system_prompt + "\n\n" + "these are the relevant chunks from the document: " + chunks
-        response = client.models.generate_content(
-            model="gemini-2.5-flash",
-            config=types.GenerateContentConfig(
-                system_instruction=system_prompt),
-                contents=chat_history
-            )
-    else:
-        response = client.models.generate_content(
-            model="gemini-2.5-flash", contents=chat_history
-        )
-    await addToChat(session_id, "model", response.text)
-    return response.text
+    # await addToChat(session_id, "user", query)
+    # chat_history = await getChatHistory(session_id)
+    # if rag:
+    #     system_prompt = open("api/logic/sysprompt.md", "r").read()
+    #     relChunks = await getQueryChunks(session_id, query)
+    #     chunks = json.dumps(relChunks["documents"])
+    #     system_prompt = system_prompt + "\n\n" + "these are the relevant chunks from the document: " + chunks
+    #     response = client.models.generate_content(
+    #         model="gemini-2.5-flash",
+    #         config=types.GenerateContentConfig(
+    #             system_instruction=system_prompt),
+    #             contents=chat_history
+    #         )
+    # else:
+    #     response = client.models.generate_content(
+    #         model="gemini-2.5-flash", contents=chat_history
+    #     )
+    # await addToChat(session_id, "model", response.text)
+    # return response.text
+
+
+    response = await provider.generate_response(query)
+    return response

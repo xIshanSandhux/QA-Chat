8cccf3b447da61a467011b609cc2dd7fe718f5a0
Ishan <ishan.sandhu3121@gmail.com>
2025-12-07 12:57:33 -0800

    got th embedding for doc, query and model initialization done

---
+++ Backend/RAG/embedding/embed.py
@@ -0,0 +1,30 @@
+# will be using BAAI embedding model for the embedding 
+
+from sentence_transformers import SentenceTransformer
+from typing import List
+
+# global variable for the model
+model = None
+
+# embedding the chunks
+async def embedDoc(chunks: List[str]):
+    global model
+    if model is None:
+        initializeEmbedModel()
+    embeddings = model.encode_document(chunks, batch_size=8)
+    return embeddings
+
+# embedding the query
+async def embedQuery(query: str):
+    global model
+    if model is None:
+        initializeEmbedModel()
+    embeddings = model.encode_query(query,batch_size=4)
+    return embeddings
+
+# initializing the embed model
+def initializeEmbedModel():
+    global model
+    if model is None:
+        model = SentenceTransformer('BAAI/bge-base-en-v1.5')
+    return model
--- Backend/embedding/embed.py
+++
@@ -1,9 +0,0 @@
-# will be using BAAI embedding model for the embedding 
-
-from sentence_transformers import SentenceTransformer
-from typing import List
-
-async def embedDoc(chunks: List[str]):
-    model = SentenceTransformer('BAAI/bge-base-en-v1.5')
-    embeddings = model.encode(chunks)
-    return embeddings
